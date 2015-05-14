bbeb2517928eda34628b6b22ed0da6d208586432
Marcus Stenbeck <marcus.stenbeck@juxt.com>
2015-05-14 12:28:58 -0700

    feat: add syphon framework to dependencies

---
+++ cefsimple/osr_widget_mac.mm
@@ -0,0 +1,9 @@
+//
+//  osr_widget_mac.m
+//  cef-cmake-externalproject-sample
+//
+//  Created by Marcus Stenbeck on 5/14/15.
+//
+//
+
+#import <Foundation/Foundation.h>
---
+++ osr_widget_mac.h
@@ -0,0 +1,13 @@
+//
+//  osr_widget_mac.h
+//  cef-cmake-externalproject-sample
+//
+//  Created by Marcus Stenbeck on 5/14/15.
+//
+//
+
+#ifndef cef_cmake_externalproject_sample_osr_widget_mac_h
+#define cef_cmake_externalproject_sample_osr_widget_mac_h
+
+
+#endif
